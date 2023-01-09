fa1272ff5a68df684d017e75a623d12d96d3861c
khalidhassanemon <khalidhassanemon12@gmail.com>
2023-01-10 00:05:31 +0600

    route-creation-fully

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -10,7 +10,7 @@ const About = () => {
                 </div>
                 <div className='md:w-1/2'>
                     <h1 className="text-5xl text-sky-700 font-bold">About us</h1>
-                    <p className="py-6">Technology services are professional services designed to facilitate the use of technology by enterprises and end users. Technology services provide specialized technology-oriented solutions by combining the processes and functions of software, hardware, networks, telecommunications and electronics.T services help provide and maintain the technology tools your office uses every day. Understanding what services IT provides can help you determine which ones you need when looking for an IT service provider. In this article, we define IT services and what they're for so you can find an IT team that works best for your organization.</p>
+                    <p className="py-6">Technology services are professional services designed to facilitate the use of technology by enterprises and end users.Technology services provide specialized technology-oriented solutions by combining the processes and functions of software, hardware, networks, telecommunications and electronics.T services help provide and maintain the technology tools your office uses every day. Understanding what services IT provides can help you determine which ones you need when looking for an IT service provider. In this article, we define IT services and what they're for so you can find an IT team that works best for your organization.</p>
                 </div>
             </div>
         </div>
